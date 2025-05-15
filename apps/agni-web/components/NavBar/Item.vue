4f52e2771441834c74300dd6226349a47236e103
Auguste Tiemele <32271140+xx-code@users.noreply.github.com>
2025-05-14 23:35:02 -0400

    migrate navbar react to vue

--- apps/agni-web/components/NavBar/Item.vue
+++ apps/agni-web/components/NavBar/Item.vue
@@ -2,23 +2,31 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import {  useRoute } from 'nuxt/app'
+import { computed, watch } from 'vue'
+
 
 library.add(fas)
 
-const props = defineProps(['link', 'icon', 'title'])
+const props = defineProps({
+    link: String,
+    icon: String,
+    title: String,
+    isResponsive: Boolean
+})
 
-const url = useRequestURL()
-const isActive =(url.pathname === props.link)
+const route = useRoute()
+const isActive = computed(() => route.path === props.link)
+
+
 
 </script>
 
 <template>
     <NuxtLink :href="link" style="text-decoration:none;" >
         <div class="link" :class="{'link-active': isActive}">
-            <div>
-                <font-awesome-icon style="margin-right:5px" :icon="icon" />
-            </div>
-            <p>{{ title }}</p>
+            <font-awesome-icon class="icon" style="" :icon="icon" />
+            <p v-if="!isResponsive">{{ title }}</p>
         </div> 
     </NuxtLink>
 </template>
@@ -26,7 +34,7 @@ const isActive =(url.pathname === props.link)
 <style scoped lang="scss">
     .link {
         color: #1E3050;
-        font-size: 0.90rem;
+        font-size: 1rem;
         border-radius: 15px;
         display: flex;
         align-items: center;
@@ -34,15 +42,23 @@ const isActive =(url.pathname === props.link)
         &:hover {
             background: rgba(103, 85, 215, 0.1);
         }
+
+        @media (max-width: 975px) {
+            justify-content: center;
+            padding: 1rem;
+        }
+
+    }
+    .icon {
+        margin-right:5px; 
+        font-size: 1rem;
+        @media (max-width: 975px) {
+            margin-right: 0; 
+        }
     }
     .link-active {
         background-color: #6755D7;
         color: #fff;
-        font-size: 0.90rem;
-        border-radius: 15px;
-        display: flex;
-        align-items: center;
-        padding-left: 20px;
         &:hover {
             background: #6755D7; 
         }
--- apps/agni-web/components/NavBar/index.vue
+++ apps/agni-web/components/NavBar/index.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
+import {useWindowSize} from '@vueuse/core'
+import { computed } from 'vue'
 
+const {width, height} = useWindowSize()
+const isResponsive = computed(() => width.value <= 975)
 </script>
 
 <template>
     <div class="nav-container">
-        
         <div class="nav-content">
             <div class="top-nav">
-                <NavBarLogoTitle title=".Agin" />
+                <NavBarLogoTitle :title="isResponsive ? 'A.' : 'Agni.'" />
             </div>
             <div>
-                <NavBarItem title="Dashboard" link="/" icon="fa-solid fa-house" />
-                <NavBarItem title="Transactions" link="/transactions" icon="fa-solid fa-money-bill-transfer" />
-                <NavBarItem title="Wallets" link="/wallets" icon="fa-solid fa-wallet" />
-                <NavBarItem title="Budgets" link="/budgets" icon="fa-solid fa-coins" />
-                <NavBarItem title="Goals" link="/goals" icon="fa-solid fa-bullseye" />
-                <NavBarItem title="Analystics" link="/analystcs" icon="fa-solid fa-chart-line" />
-                <NavBarItem title="Settings" link="/settings" icon="fa-solid fa-gear" />
+                <NavBarItem title="Dashboard" link="/" icon="fa-solid fa-house" :isResponsive="isResponsive" />
+                <NavBarItem title="Transactions" link="/transactions" icon="fa-solid fa-money-bill-transfer" :isResponsive="isResponsive"/>
+                <NavBarItem title="Wallets" link="/wallets" icon="fa-solid fa-wallet" :isResponsive="isResponsive" />
+                <NavBarItem title="Budgets" link="/budgets" icon="fa-solid fa-coins" :isResponsive="isResponsive" />
+                <NavBarItem title="Goals" link="/goals" icon="fa-solid fa-bullseye" :isResponsive="isResponsive" />
+                <NavBarItem title="Analystics" link="/analystics" icon="fa-solid fa-chart-line" :isResponsive="isResponsive"/>
+                <NavBarItem title="Settings" link="/settings" icon="fa-solid fa-gear" :isResponsive="isResponsive" />
             </div>
         </div>
     </div>
@@ -27,10 +30,15 @@
     margin-bottom: 75px;
     margin-left: 1em;
     margin-right: 1em;
+    transition: 250ms ease-out;
 }
 .nav-container {
     background-color: rgba(103, 85, 215, 0.1);
     width: 225px;
+    @media (max-width: 975px) {
+        width: 75px;
+    };
+    transition: 250ms ease-out;
 }
 .nav-content {
     padding: 1rem;
