0b936b364f90229c29e8abbce351f7cd772f3134
Auguste Tiemele <32271140+xx-code@users.noreply.github.com>
2026-02-22 19:30:50 -0500

    feat: Add a compute occurence

--- apps/agni_api/src/main/kotlin/dev/auguste/agni_api/core/value_objects/SchedulerRecurrence.kt
+++ apps/agni_api/src/main/kotlin/dev/auguste/agni_api/core/value_objects/SchedulerRecurrence.kt
@@ -1,11 +1,23 @@
 package dev.auguste.agni_api.core.value_objects
 
 import dev.auguste.agni_api.core.entities.enums.PeriodType
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 
 data class SchedulerRecurrence(val period: PeriodType, val interval: Int) {
     fun toMap(): Map<String, Any?> {
         return mapOf("period" to period, "interval" to interval)
+    }
+
+    fun computeOccurrences(startDate: LocalDate, endDate: LocalDate): Int {
+        val occurrences = when (period) {
+            PeriodType.YEAR -> ChronoUnit.YEARS.between(startDate, endDate)
+            PeriodType.MONTH -> ChronoUnit.MONTHS.between(startDate, endDate)
+            PeriodType.WEEK -> ChronoUnit.WEEKS.between(startDate, endDate)
+            PeriodType.DAY -> ChronoUnit.DAYS.between(startDate, endDate)
+        }
+        return (occurrences / interval).toInt()
     }
 
     companion object {
