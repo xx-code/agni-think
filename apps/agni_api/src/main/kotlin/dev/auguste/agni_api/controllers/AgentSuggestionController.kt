b3d34e09b83f993d35e0f964ca77ebc45ca63dd1
Auguste Tiemele <32271140+xx-code@users.noreply.github.com>
2026-06-14 00:41:14 -0400

    Revert "change name repo to avoid docker cache"

--- apps/agni_api/src/main/kotlin/dev/auguste/agni_api/controllers/AgentSuggestionController.kt
+++ apps/agni_api/src/main/kotlin/dev/auguste/agni_api/controllers/AgentSuggestionController.kt
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-// Modif alert
-
 @RestController
 @RequestMapping("/v2/agent-suggestions")
 class AgentSuggestionController(
